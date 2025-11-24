4a13523da39b7d568379fc80644d5ffd4d2b18cc
adelipingu14 <adelipingu14@gmail.com>
2025-11-24 12:50:52 +0900

    Step1: 스탭1 커밋

---
+++ Assets/Scripts/UserData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserData : MonoBehaviour
+{
+    public Text userName;
+    public Text money;
+    public Text accountmoney;
+}
